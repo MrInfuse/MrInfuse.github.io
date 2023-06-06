2c45096c4abadec2de78aa2819ee17dacf802fb1
John Dereck Timbreza <39250321+MrInfuse@users.noreply.github.com>
2023-06-06 10:44:02 +0800

    update

--- nivo-slider/scripts/scripts.js
+++ nivo-slider/scripts/scripts.js
@@ -1,6 +1,30 @@
-$(window).load(function() {
-    $('#slider').nivoSlider({
-        boxCols: 8,
-        boxRows: 4,
-    });
-});
+  /*Navigation menu button*/
+  var navButton = document.querySelector(".navigation-menu-button img");
+  if (navButton.addEventListener) {
+     navButton.addEventListener("click", function() {
+        var nav = document.querySelector(".sitenavigation ul");
+        if (nav.style.display === "block") {
+           nav.style.display = "";
+        } else {
+           nav.style.display = "block";
+        }
+     }, false);
+  } else if (navButton.attachEvent) {
+     navButton.attachEvent("onclick", function() {
+        var nav = document.querySelector(".sitenavigation ul");
+        if (nav.style.display === "block") {
+           nav.style.display = "";
+        } else {
+           nav.style.display = "block";
+        }
+     });
+  }
+  function abc(name){
+       var div = document.getElementById(name);
+      if (div.style.display !== 'none') {
+          div.style.display = 'none';
+      }
+      else {
+          div.style.display = 'block';
+      }
+  }
